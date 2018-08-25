eab4c5326864330cf0293f981e422d4454308264
OlgaKulina <kulinaolg@gmail.com>
2018-08-25 15:19:38 +0300

    Change_new string

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -11,7 +11,15 @@ namespace ConsoleApp2
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Text");
+            Console.WriteLine("Text0");
+
+            Console.WriteLine("Text1");
+
+            Console.WriteLine("Text2");
+
+            Console.ReadLine();
+
+
         }
     }
 }
